875822f7429c719b3a9d7ad639922486f029dbcb
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-02-12 22:19:16 +0100

    Integration tests for the api/runCalculator get

--- src/main/java/com/blackiceinc/era/services/RunCalculatorService.java
+++ src/main/java/com/blackiceinc/era/services/RunCalculatorService.java
@@ -1,162 +1,23 @@
 package com.blackiceinc.era.services;
 
-import com.blackiceinc.era.persistence.erau.model.MeasurementSensitivity;
 import com.blackiceinc.era.persistence.erau.model.RunCalculator;
-import com.blackiceinc.era.persistence.erau.repository.RunCalculatorRepository;
-import com.blackiceinc.era.persistence.erau.specifications.RunCalculatorSpecificationsBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.sql.Date;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
-@Service
-public class RunCalculatorService {
+/**
+ * Created by tmanev on 2/12/2016.
+ */
+public interface RunCalculatorService {
+    Map<String, List> getFilterOptions() throws SQLException;
 
-    private static final String SNAPSHOT_DATE = "snapshotDate";
-    private static final String LOAD_JOB_NBR = "loadJobNbr";
-    private static final String SCENARIO_ID = "scenarioId";
-    public static final int PAGE_0 = 0;
-    public static final int PAGE_SIZE_25 = 25;
-
-    private final Logger log = LoggerFactory.getLogger(RunCalculatorService.class);
-
-    @Autowired
-    private Environment env;
-
-    @Autowired
-    private RunCalculatorRepository runCalculatorRepository;
-
-    public Map<String, List> getFilterOptions() throws SQLException {
-        Map<String, List> filterOptions = new HashMap<>();
-
-        List<Date> snapshotDateList = new ArrayList<>();
-        List<BigDecimal> loadJobNbrList = new ArrayList<>();
-        List<String> scenarioIdList = new ArrayList<>();
-
-        fillFilterFromMeasurementSensitivityDB(snapshotDateList, loadJobNbrList, scenarioIdList);
-
-        filterOptions.put(SNAPSHOT_DATE, snapshotDateList);
-        filterOptions.put(LOAD_JOB_NBR, loadJobNbrList);
-        filterOptions.put(SCENARIO_ID, scenarioIdList);
-
-        return filterOptions;
-    }
-
-    public Page<RunCalculator> findRunCalculationByParams(Integer page,
-                                                          Integer length,
-                                                          Date snapshotDate,
-                                                          BigDecimal loadJobNbr,
-                                                          String scenarioId) {
-        Specification<RunCalculator> specRunCalculator = getRunCalculatorSpecification(snapshotDate, loadJobNbr, scenarioId);
-
-        int pageNumber = (page != null) ? page : PAGE_0;
-        int pageSize = (length != null) ? length : PAGE_SIZE_25;
-
-        return runCalculatorRepository.findAll(specRunCalculator, new PageRequest(pageNumber, pageSize));
-    }
-
-    private Specification<RunCalculator> getRunCalculatorSpecification(Date snapshotDate,
-                                                                       BigDecimal loadJobNbr,
-                                                                       String scenarioId) {
-        RunCalculatorSpecificationsBuilder builder = new RunCalculatorSpecificationsBuilder();
-        if (snapshotDate != null) {
-            builder.with("snapshotDate", ":", snapshotDate, "", "");
-        }
-
-        if (loadJobNbr != null) {
-            builder.with("loadJobNbr", ":", loadJobNbr, "", "");
-        }
-
-        if (scenarioId != null) {
-            builder.with("scenarioId", ":", scenarioId, "", "");
-        }
-
-        return builder.build();
-    }
-
-    private void fillFilterWithDummyData(List<Date> snapshotDateList, List<BigDecimal> loadJobNbrList, List<String> scenarioIdList) {
-        for (MeasurementSensitivity ms : generateDummyMeasurementData()) {
-            if (!snapshotDateList.contains(ms.getSnapshotDate())) {
-                snapshotDateList.add(ms.getSnapshotDate());
-            }
-
-            if (!loadJobNbrList.contains(ms.getLoadJobNbr())) {
-                loadJobNbrList.add(ms.getLoadJobNbr());
-            }
-
-            if (!scenarioIdList.contains(ms.getScenarioId())) {
-                scenarioIdList.add(ms.getScenarioId());
-            }
-        }
-    }
-
-    private void fillFilterFromMeasurementSensitivityDB(List<Date> snapshotDateList, List<BigDecimal> loadJobNbrList, List<String> scenarioIdList) throws SQLException {
-        Connection conn = DriverManager.getConnection(
-                env.getProperty("jdbc.url"),
-                env.getProperty("jdbc.user"),
-                env.getProperty("jdbc.pass"));
-        conn.setAutoCommit(false);
-
-        Statement stmt = conn.createStatement();
-
-        long start = System.currentTimeMillis();
-        log.info("Starting taking MEASUREMENT_SENSITIVITY data");
-        ResultSet resultSet = stmt.executeQuery("select DISTINCT SNAPSHOT_DATE, LOAD_JOB_NBR, SCENARIO_ID from MEASUREMENT_SENSITIVITY");
-        log.info("MEASUREMENT_SENSITIVITY data took {} ms", System.currentTimeMillis() - start);
-        while (resultSet.next()) {
-            Date snapshotDate = resultSet.getDate("SNAPSHOT_DATE");
-            BigDecimal loadJobNbr = resultSet.getBigDecimal("LOAD_JOB_NBR");
-            String scenarioId = resultSet.getString("SCENARIO_ID");
-
-            if (!snapshotDateList.contains(snapshotDate)) {
-                snapshotDateList.add(snapshotDate);
-            }
-
-            if (!loadJobNbrList.contains(loadJobNbr)) {
-                loadJobNbrList.add(loadJobNbr);
-            }
-
-            if (!scenarioIdList.contains(scenarioId)) {
-                scenarioIdList.add(scenarioId);
-            }
-        }
-    }
-
-    private List<MeasurementSensitivity> generateDummyMeasurementData() {
-        List<MeasurementSensitivity> result = new ArrayList<MeasurementSensitivity>();
-
-        result.add(createMeasurementSensitivity(new Date(116, 0, 1), new BigDecimal(1), "scen1"));
-        result.add(createMeasurementSensitivity(new Date(116, 1, 1), new BigDecimal(2), "scen2"));
-        result.add(createMeasurementSensitivity(new Date(116, 1, 1), new BigDecimal(3), "scen2"));
-        result.add(createMeasurementSensitivity(new Date(116, 2, 1), new BigDecimal(3), "scen3"));
-        result.add(createMeasurementSensitivity(new Date(116, 3, 1), new BigDecimal(4), "scen4"));
-        result.add(createMeasurementSensitivity(new Date(116, 4, 1), new BigDecimal(4), "scen4"));
-        result.add(createMeasurementSensitivity(new Date(116, 5, 1), new BigDecimal(10), "scen5"));
-        result.add(createMeasurementSensitivity(new Date(116, 6, 1), new BigDecimal(10), "scen6"));
-        result.add(createMeasurementSensitivity(new Date(116, 7, 1), new BigDecimal(11), "scen7"));
-        result.add(createMeasurementSensitivity(new Date(116, 8, 1), new BigDecimal(13), "scen7"));
-
-        return result;
-    }
-
-    private MeasurementSensitivity createMeasurementSensitivity(Date date, BigDecimal jobNbr, String scenarioId) {
-        MeasurementSensitivity ms1 = new MeasurementSensitivity();
-        ms1.setSnapshotDate(date);
-        ms1.setLoadJobNbr(jobNbr);
-        ms1.setScenarioId(scenarioId);
-
-        return ms1;
-    }
-
+    Page<RunCalculator> findRunCalculationByParams(Integer page,
+                                                   Integer length,
+                                                   Date snapshotDate,
+                                                   BigDecimal loadJobNbr,
+                                                   String scenarioId);
 }
--- src/test/java/com/blackiceinc/web/rest/RunCalculatorResourceTest.java
+++ src/test/java/com/blackiceinc/web/rest/RunCalculatorResourceTest.java
@@ -1,15 +1,20 @@
 package com.blackiceinc.web.rest;
 
 import com.blackiceinc.era.BlackiceincEraApplication;
+import com.blackiceinc.era.persistence.erau.model.RunCalculator;
 import com.blackiceinc.era.persistence.erau.repository.RunCalculatorRepository;
+import com.blackiceinc.era.services.RunCalculatorService;
 import com.blackiceinc.era.web.rest.RunCalculatorResource;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -19,6 +24,17 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasToString;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = BlackiceincEraApplication.class)
@@ -31,6 +47,9 @@ public class RunCalculatorResourceTest {
     @Autowired
     private RunCalculatorRepository runCalculatorRepository;
 
+    @Mock
+    private RunCalculatorService runCalculatorService;
+
     @Autowired
     private MappingJackson2HttpMessageConverter jacksonMessageConverter;
 
@@ -39,14 +58,36 @@ public class RunCalculatorResourceTest {
         MockitoAnnotations.initMocks(this);
         RunCalculatorResource runCalculatorResource = new RunCalculatorResource();
         ReflectionTestUtils.setField(runCalculatorResource, "runCalculatorRepository", runCalculatorRepository);
+        ReflectionTestUtils.setField(runCalculatorResource, "runCalculatorService", runCalculatorService);
 
         this.restRunCalculatorMockMvc = MockMvcBuilders.standaloneSetup(runCalculatorResource).setMessageConverters(jacksonMessageConverter).build();
     }
 
     @Test
     @Transactional
-    public void getAllRunCalculator() {
+    public void getAllRunCalculator() throws Exception {
+        ArrayList<RunCalculator> runCalculators = new ArrayList<>();
 
+        RunCalculator runCalculator = new RunCalculator();
+        runCalculator.setId(1l);
+        runCalculator.setSnapshotDate(new Date(Calendar.getInstance().getTimeInMillis()));
+        runCalculator.setLoadJobNbr(1l);
+        runCalculator.setScenarioId("1");
+        runCalculators.add(runCalculator);
+
+
+        Page<RunCalculator> page = new PageImpl<RunCalculator>(runCalculators);
+        when(runCalculatorService.findRunCalculationByParams(anyInt(), anyInt(), any(Date.class), any(BigDecimal.class),
+                anyString())).thenReturn(page);
+
+        // Get runCalculation
+        restRunCalculatorMockMvc.perform(get("/api/runCalculator"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content.[*].id").value(hasItem(runCalculator.getId().intValue())))
+                .andExpect(jsonPath("$.content.[*].snapshotDate").value(hasItem(runCalculator.getSnapshotDate().toString())))
+                .andExpect(jsonPath("$.content.[*].loadJobNbr").value(hasItem(runCalculator.getLoadJobNbr().intValue())))
+                .andExpect(jsonPath("$.content.[*].scenarioId").value(hasItem(runCalculator.getScenarioId())));
     }
 
 }

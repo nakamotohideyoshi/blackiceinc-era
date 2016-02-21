501d93b04ea61bf13a8bd034a63dcee7ed448c20
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-02-21 21:23:57 +0100

    Added Account Settings screen with controller support.

---
+++ src/main/webapp/resources/js/ng/controllers/ng.account-settings.controller.js
@@ -0,0 +1,8 @@
+angular.module('ng.account-settings.controller', [])
+  .controller('AccountSettingsController', ['$scope', '$timeout', 'AccountSettingsService',
+    'Util', '$routeParams', 'VNotificationService', '$location', 'ConfirmService',
+    function($scope, $timeout, AccountSettingsService, Util, $routeParams,
+      VNotificationService, $location, ConfirmService) {
+
+    }
+  ]);

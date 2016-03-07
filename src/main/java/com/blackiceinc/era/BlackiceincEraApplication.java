10d14305d4d8c7aea20eace0e9b4318520878bc9
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-03-07 13:57:32 +0100

    Added profiles for different environments.

--- src/main/java/com/blackiceinc/era/BlackiceincEraApplication.java
+++ src/main/java/com/blackiceinc/era/BlackiceincEraApplication.java
@@ -1,13 +1,59 @@
 package com.blackiceinc.era;
 
+import com.blackiceinc.era.config.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 
 @SpringBootApplication//(exclude = {SecurityAutoConfiguration.class })
 public class BlackiceincEraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlackiceincEraApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(BlackiceincEraApplication.class);
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void initApplication() {
+        if (env.getActiveProfiles().length == 0) {
+            log.warn("No Spring profile configured, running with default configuration");
+        } else {
+            log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+        }
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(BlackiceincEraApplication.class);
+
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        addDefaultProfile(app, source);
+
+        Environment env = app.run(args).getEnvironment();
+        log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+
+    /**
+     * If no profile has been configured, set by default the "dev" profile.
+     */
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active") &&
+                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+
+            app.setAdditionalProfiles(Constants.SPRING_PROFILE_LOCAL);
+        }
+    }
 }
---
+++ src/main/java/com/blackiceinc/era/config/Constants.java
@@ -0,0 +1,8 @@
+package com.blackiceinc.era.config;
+
+public final class Constants {
+
+    public static final String SPRING_PROFILE_DEV_ERA = "dev_era";
+    public static final String SPRING_PROFILE_LOCAL = "local";
+
+}

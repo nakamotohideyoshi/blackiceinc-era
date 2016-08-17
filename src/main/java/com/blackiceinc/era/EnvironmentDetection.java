119868a374cdc5b7bc4b9b2e3207ef1c452e9ae8
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-08-17 09:20:33 +0200

    hostname to be compared with all letters to upper.

--- src/main/java/com/blackiceinc/era/EnvironmentDetection.java
+++ src/main/java/com/blackiceinc/era/EnvironmentDetection.java
@@ -43,13 +43,14 @@ public class EnvironmentDetection {
     private static String getProfileByIpAddress(String hostName) {
         String profile;
 
-        if (hostName.startsWith(HOST_ERA_BLACKICEINC_COM)) {
+        String hostNameUpper = hostName.toUpperCase();
+        if (hostNameUpper.startsWith(HOST_ERA_BLACKICEINC_COM)) {
             profile = Constants.SPRING_PROFILE_DEV_ERA;
-        } else if (hostName.startsWith(HOST_DEV_BASEL2_COGNOS)) {
+        } else if (hostNameUpper.startsWith(HOST_DEV_BASEL2_COGNOS)) {
             profile = Constants.SPRING_PROFILE_ONSITE;
-        } else if (hostName.startsWith(HOST_DC_BASEL2_COGNOS)) {
+        } else if (hostNameUpper.startsWith(HOST_DC_BASEL2_COGNOS)) {
             profile = Constants.SPRING_PROFILE_PRODUCTION;
-        } else if (hostName.startsWith(HOST_DR_BASEL2_COGNOS)) {
+        } else if (hostNameUpper.startsWith(HOST_DR_BASEL2_COGNOS)) {
             profile = Constants.SPRING_PROFILE_RECOVERY;
         } else {
             profile = Constants.SPRING_PROFILE_LOCAL;
